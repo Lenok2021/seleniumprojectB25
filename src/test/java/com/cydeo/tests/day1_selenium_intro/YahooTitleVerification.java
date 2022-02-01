52f9ff1d59d23391640348195f5415f60b000bc5
cross <crosstown11223@yahoo.com>
2022-02-01 16:54:33 -0500

    initial commit

--- src/test/java/com/cydeo/tests/day1_selenium_intro/YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/YahooTitleVerification.java
@@ -9,21 +9,31 @@ public class YahooTitleVerification {
     public static void main(String[] args) {
 
 
+        // WE  DO testing we HAVE  to do it step by step
+
         //TC #1: Yahoo Title Verification
         // do setup for browser driver
         WebDriverManager.chromedriver().setup();
 
         //1.Open Chrome browser
         WebDriver driver = new ChromeDriver();
+
+        // make our page full screen
+        driver.manage().window().maximize();
+
+
         //REMEMBER  WHEN YOU WROTE TWO LINES CODES RUN IT CHECK YOURSELF
 
         //2.Go to https://www.yahoo.com
         driver.get("https://www.yahoo.com");
+
+
         //3. Verify title:
         //Expected: Yahoo
 
 
-// WE  DO testing we HAVE  to do it step by step
+
+
 
 
     }
