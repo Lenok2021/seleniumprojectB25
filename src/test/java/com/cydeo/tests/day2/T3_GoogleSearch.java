baea691cbdf84dfbd7d142163ef27d1f1c8a3607
cross <crosstown11223@yahoo.com>
2022-02-04 14:45:32 -0500

    initial commit

--- src/test/java/com/cydeo/tests/day2/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2/T3_GoogleSearch.java
@@ -1,10 +1,38 @@
 package com.cydeo.tests.day2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T3_GoogleSearch {
     public static void main(String[] args) {
 
+        //1- Open a Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //2- Go to: https://google.com
+        driver.get(" https://google.com");
 
+        //3 write "apple" in search box
+        // 4 press ENTER  using Keys.Enter
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
 
+        //verify title:
+        // Expected
+
+        String   expectedTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.startsWith(expectedTitle)){
+            System.out.println("Title verification PASSED");
+        }else{
+            System.out.println("Title verification FAILED");
+        }
 
     }
 }
