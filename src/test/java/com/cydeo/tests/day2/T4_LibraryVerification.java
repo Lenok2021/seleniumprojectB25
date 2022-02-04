dca457671c0f81e88b57a574b7f933ad2f0d4da4
cross <crosstown11223@yahoo.com>
2022-02-04 15:48:43 -0500

    LibrayLoginPage

--- src/test/java/com/cydeo/tests/day2/T4_LibraryVerification.java
+++ src/test/java/com/cydeo/tests/day2/T4_LibraryVerification.java
@@ -1,14 +1,16 @@
 package com.cydeo.tests.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T4_LibraryVerification {
 
     public static void main(String[] args) {
 
-
+        // we are  doing negative testing
         // 1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -18,9 +20,20 @@ public class T4_LibraryVerification {
         driver.get("http://library2.cybertekschool.com/login.html");
 
         // 3. Enter username: “incorrect@email.com”
+        WebElement userNameInput = driver.findElement(By.className("form-control"));
+        userNameInput.sendKeys("incorrect@email.com");
 
 
         // 4. Enter password: “incorrect password”
+        WebElement userPassword = driver.findElement(By.id("inputPassword"));
+        userPassword.sendKeys("incorrect password");
+
+
+        // 5. Click to Sign in Bottom
+        WebElement sighInBottom = driver.findElement(By.tagName("bottom"));
+
+
+
       /* 5. Verify: visually “Sorry, Wrong Email or Password” displayed
         PS: Locate username input box using “className” locator
         Locate password input box using “id” locator
