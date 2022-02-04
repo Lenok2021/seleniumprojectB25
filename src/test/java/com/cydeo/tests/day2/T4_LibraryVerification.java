ca2b32c39db2e9e52cfe933c68c7f4edcff41413
cross <crosstown11223@yahoo.com>
2022-02-04 16:10:16 -0500

    LibrayLoginPage

--- src/test/java/com/cydeo/tests/day2/T4_LibraryVerification.java
+++ src/test/java/com/cydeo/tests/day2/T4_LibraryVerification.java
@@ -10,38 +10,28 @@ public class T4_LibraryVerification {
 
     public static void main(String[] args) {
 
-        // we are  doing negative testing
-        // 1. Open Chrome browser
+        //TC #4: Library verifications
+        //1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        //2. Go to https://library2.cybertekschool.com/login.html
+        driver.get("https://library2.cybertekschool.com/login.html");
 
-        // 2. Go to http://library2.cybertekschool.com/login.html
-        driver.get("http://library2.cybertekschool.com/login.html");
+        //3. Enter username: “incorrect@email.com”
+        WebElement usernameInput = driver.findElement(By.className("form-control"));
+        usernameInput.sendKeys("incorrect@email.com");
 
-        // 3. Enter username: “incorrect@email.com”
-        WebElement userNameInput = driver.findElement(By.className("form-control"));
-        userNameInput.sendKeys("incorrect@email.com");
+        //4. Enter password: “incorrect password”
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
 
+        //5. Click to Sign in button
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
 
-        // 4. Enter password: “incorrect password”
-        WebElement userPassword = driver.findElement(By.id("inputPassword"));
-        userPassword.sendKeys("incorrect password");
-
-
-        // 5. Click to Sign in Bottom
-        WebElement sighInBottom = driver.findElement(By.tagName("bottom"));
-        sighInBottom.click();
-
-
-
-      /* 5. Verify: visually “Sorry, Wrong Email or Password” displayed
-        PS: Locate username input box using “className” locator
-        Locate password input box using “id” locator
-        Locate Sign in button using “tagName” locator
-      */
-
-
+        //6. Verify: visually “Sorry, Wrong Email or Password”
+        //displayed
 
 
     }
