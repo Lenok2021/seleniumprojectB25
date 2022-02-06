9b3c0dd9c842a3241e7ddec909dde1bb420185c4
cross <crosstown11223@yahoo.com>
2022-02-06 17:55:43 -0500

    LibrayLoginPage

---
+++ src/test/java/com/cydeo/tests/practice/CydeoTask.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CydeoTask {
+
+    public static void main(String[] args) {
+
+        //TC #1: Cydeo practice tool verifications
+
+        // 1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Go to https://practice.cydeo.com
+        driver.get("https://practice.cydeo.com ");
+
+        //3. Verify URL contains
+        // Expected: cydeo
+
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+        if (actualURL.contains(expectedURL)) {
+            System.out.println("PASSED!!!");
+        } else {
+            System.out.println("PASSED !!!");
+        }
+        // 4. Verify title:
+        // Expected: Practice
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("PASSED!!!");
+        }else{
+            System.out.println("FAILED");
+        }
+
+
+    }
+}
