63a58c4eed7f8b40079ee7d965bbc4ebfdf0e67e
cross <crosstown11223@yahoo.com>
2022-02-06 19:08:47 -0500

    LibrayLoginPage

---
+++ src/test/java/com/cydeo/tests/practice/Apple.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Apple {
+    public static void main(String[] args) {
+     /*
+        1- Open a chrome browser
+2- Go to: https://google.com
+3- Write “apple” in search box
+4- Click google search button
+5- Verify title:
+Expected: Title should start with “apple” word
+      */
+
+      //  1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+       // 2- Go to: https://google.com
+        driver.get("https://google.com");
+
+       // 3 Find inputBox
+        // PRESS KEYS
+        WebElement inputBox = driver.findElement(By.name("q"));
+        inputBox.sendKeys("Apple" + Keys.ENTER);
+
+
+       // 4- Verify title:
+      //  Expected: Title should start with “apple” word
+
+
+    }
+}
--- src/test/java/com/cydeo/tests/practice/CydeoTask.java
+++ src/test/java/com/cydeo/tests/practice/CydeoTask.java
@@ -39,5 +39,7 @@ public class CydeoTask {
         }
 
 
+        driver.close();
+
     }
 }
